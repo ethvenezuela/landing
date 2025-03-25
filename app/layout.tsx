4d93f014daf55d103b65371f9a00b8fda01f6167
Ruben Santana <rubensantana@outlook.com>
2025-03-25 19:03:38 -0400

    update icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,18 @@ import type React from "react"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Ethereum Venezuela",
-  description: "Comunidad de Ethereum en Venezuela"
+  description: "Comunidad de Ethereum en Venezuela",
+  icons: [
+    {
+      url: '/favicon.png',
+      rel: 'icon',
+      type: 'image/x-icon',
+    },
+  ],
 }
 
 export default function RootLayout({
